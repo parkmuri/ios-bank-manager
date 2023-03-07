6623c761834183411a2025c0bee12f8a5390a2d4
parkmuri <smxhmu@naver.com>
2023-03-07 11:09:34 +0900

    feat: client 타입 생성 및 구현

---
+++ BankManagerConsoleApp/Model/Bank.swift
@@ -0,0 +1,8 @@
+//
+//  Bank.swift
+//  BankManagerConsoleApp
+//
+//  Created by 박소연 on 2023/03/07.
+//
+
+import Foundation
---
+++ BankManagerConsoleApp/Model/Client.swift
@@ -0,0 +1,8 @@
+//
+//  Client.swift
+//  BankManagerConsoleApp
+//
+//  Created by 박소연 on 2023/03/07.
+//
+
+import Foundation
